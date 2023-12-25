b19b55d3a636d9a2a22c9bd7058140a340bcce8e
Sasi chandan <bhargavsasichandan.com>
2023-12-25 15:16:08 +0530

    first

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
     
     try {
       const response = await fetch(
-        `https://portfolioback-4hta.onrender.com/api/auth/contact`,
+        `https://https://portfolioback-4hta.onrender.com/api/auth/contact`,
         {
           method: "POST",
           headers: {
